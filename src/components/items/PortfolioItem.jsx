eeaf0cba52aed4826c74e14311a97c1f7b9ebac9
GDK1995 <87063068+GDK1995@users.noreply.github.com>
2024-11-15 00:22:27 +0600

    scroll

--- src/components/items/PortfolioItem.jsx
+++ src/components/items/PortfolioItem.jsx
@@ -1,45 +1,66 @@
+import { useRef } from 'react'
 import { PORTFOLIO_LIST } from '../../store/constants'
 import { IoIosArrowBack } from "react-icons/io"
 import { IoIosArrowForward } from "react-icons/io"
 
 function PortfolioItem () {
-  const scrolling = document.getElementById('scroll')
-  console.log(scrolling)
+  const scrlRef = useRef(null)
+  let intervaling = null
 
-  let scrollToken = null
-
-  function ScrollDiv (direction) {
-    const scroll = document.getElementById('scroll')
-
-    scrollToken = setInterval(function(){
-      if(direction === 'left'){
-        scroll.scrollBy({ left: 10, behavior: 'smooth' })
-      } else {
-        scroll.scrollBy({ left: -10, behavior: 'smooth' })
-      }
-    }, 2,)
+  const MouseEnter = (direction) => {
+    intervaling = setInterval(() => {
+      direction === 'left' ? scrlRef.current.scrollBy({ left: 10, behavior: 'smooth' }) : scrlRef.current.scrollBy({ left: -10, behavior: 'smooth' })
+    }, 2)
   }
 
-  function stopScroll(){
-    console.log('ghj', scrollToken)
-    clearInterval(scrollToken)
-    console.log('end', scrollToken)
+  const MouseLeave = () => {
+    clearInterval(intervaling)
+    console.log(intervaling)
   }
+
+  // useEffect(() => {
+  //   const handleMouseLeftMove = () => {
+  //     intervaling = setInterval(() => {
+  //       scrlRef.current.scrollBy({ left: 10, behavior: 'smooth' })
+  //     }, 2);
+  //   }
+
+  //   const handleMouseRightMove = () => {
+  //     scrlRef.current.scrollBy({ left: -10, behavior: 'smooth' })
+  //   }
+
+  //   lftbtnRef.current.addEventListener('mouseover', handleMouseLeftMove)
+  //   rghtbtnRef.current.addEventListener('mouseover', handleMouseRightMove)
+
+  //   return () => clearInterval(intervaling)
+  // }, [])
+
+  // function ScrollDiv (direction) {
+  //   const scroll = document.getElementById('scroll')
+  //   const scrollToken = setInterval(function(){
+  //     if(direction === 'left'){
+  //       scroll.scrollBy({ left: 10, behavior: 'smooth' })
+  //     } else {
+  //       scroll.scrollBy({ left: -10, behavior: 'smooth' })
+  //     }
+  //   }, 2)
+  //   handleScroll(scrollToken)
+  // }
 
   return (
     <>
       <div className='relative w-[940px]'>
         <li className='absolute left-0 z-10 top-0 bottom-0 flex'>
-          <button onMouseOver={() => ScrollDiv('left')} onMouseLeave={() => stopScroll()} className='px-4 m-auto rounded-full bg-gray-500 hover:bg-gray-400 opacity-80 h-[52px] w-[52px] flex'>
+          <button onMouseEnter={() => MouseEnter('left')} onMouseLeave={() => MouseLeave} id='left' className='px-4 m-auto rounded-full bg-gray-500 hover:bg-gray-400 opacity-80 h-[52px] w-[52px] flex'>
             <IoIosArrowBack className='m-auto' fill="white" size={20}/>
           </button>
         </li>
         <li className='absolute right-0 z-10 top-0 bottom-0 flex'>
-          <button onMouseOver={() => ScrollDiv('right')} onMouseLeave={() => stopScroll()} className='px-4 m-auto rounded-full bg-gray-500 hover:bg-gray-400 opacity-80 h-[52px] w-[52px] flex'>
+          <button onMouseEnter={() => MouseEnter('right')} onMouseLeave={() => MouseLeave} className='px-4 m-auto rounded-full bg-gray-500 hover:bg-gray-400 opacity-80 h-[52px] w-[52px] flex'>
             <IoIosArrowForward className='m-auto' fill="white" size={20}/>
           </button>
         </li>
-        <ul id='scroll' className='overflow-x-auto overflow-y-hidden whitespace-nowrap [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
+        <ul ref={scrlRef} className='overflow-x-auto overflow-y-hidden whitespace-nowrap [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
           {PORTFOLIO_LIST.map((photo, index) =>
             <li
               key={photo + index}
